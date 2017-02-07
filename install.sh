d60f6c7eff3751df3c934e72a7a9cf92c4cd4615
James Newton <hello@jamesnewton.com>
2017-02-07 14:21:41 -0500

    move zshrc and tmux.conf into their directories

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 echo 'Installing dotfiles...'
 
 echo ' - Linking zsh files'
-ln -s .zshrc $HOME/.zshrc
+ln -s zsh/.zshrc $HOME/.zshrc
 touch $HOME/.zsh_custom
 mkdir -p $HOME/.oh-my-zsh/custom/themes/bureau-james.zsh-theme zsh/custom/themes/bureau-james.zsh-theme
 
 echo ' - Linking tmux config'
-ln -s .tmux.conf $HOME/.tmux.conf
+ln -s tmux/.tmux.conf $HOME/.tmux.conf
 
 echo ' - Linking global git config'
 ln -s .gitconfig $HOME/.gitconfig
