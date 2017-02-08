4d4e53884b3c6d2063a63255c4e59105f3f89586
James Newton <hello@jamesnewton.com>
2017-02-08 11:31:17 -0500

    add heavily modified semaphore script

--- bitbar/install.sh
+++ bitbar/install.sh
@@ -1,1 +1,2 @@
 go build -o enabled/twitch.5m.cgo twitch.5m.go
+ln -s $PWD/semaphore.15m.js $PWD/enabled/semaphore.15m.js
---
+++ bitbar/semaphore.15m.js
@@ -0,0 +1,55 @@
+#!/usr/local/bin/node
+
+/* jshint esversion: 6 */
+
+const AUTH_TOKEN = process.env.SEMAPHORE_CI_AUTH_TOKEN;
+const AUTHOR_NAME = 'James Newton';
+const url = 'https://semaphoreci.com/api/v1/projects?auth_token=' + AUTH_TOKEN;
+const https = require('https');
+
+var icon = '✔';
+
+function color(result) {
+    if (result == 'passed') {
+        return 'green';
+    } else if (result == 'pending') {
+        return 'orange';
+    } else {
+        return 'red';
+    }
+}
+
+function branchesOutput(branches) {
+    branches = branches.filter(branch => {
+        return branch.commit && branch.commit.author_name === AUTHOR_NAME;
+    });
+
+    return branches.map(branch => {
+        if (branch.result === 'failed') {
+            icon = '✖';
+        }
+
+        return `${branch.branch_name} | size=12 color=${color(branch.result)} href=${branch.build_url}`;
+    }).join('\n');
+}
+
+function handleResponse(body) {
+    let output = body.map(project => {
+        return `${project.owner}/${project.name} | size=16 href=${project.html_url}\n${branchesOutput(project.branches)}`;
+    }).join('\n---\n');
+
+    console.log(`${icon} Semaphore CI\n---\n${output}`);
+}
+
+https.get(url, res => {
+    let body = '';
+
+    res.on('data', data => {
+        body += data;
+    });
+
+    res.on('end', () => {
+        handleResponse(JSON.parse(body));
+    });
+});
+
