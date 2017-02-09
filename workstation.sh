0fb8c9772982cf8247c2137ed5d9ec3752836274
James Newton <hello@jamesnewton.com>
2017-02-09 14:04:02 -0500

    update symlink paths

--- workstation.sh
+++ workstation.sh
@@ -5,17 +5,17 @@ puts "Setting up Workstation"
 brew install fasd
 
 brew install git
-ln -s .gitconfig $HOME/.gitconfig
+ln -s $PWD/.gitconfig $HOME/.gitconfig
 
 brew install vim --with-lua --with-luajit
-ln -s vim/.vimrc $HOME/.vimrc
+ln -s $PWD/vim/.vimrc $HOME/.vimrc
 git clone http://github.com/VundleVim/Vundle.vim vim/bundle/Vundle.vim
 vim +PluginInstall +qa
 
 brew install zsh
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-rm $HOME/.zshrc && ln -s zsh/.zshrc $HOME/.zshrc
+rm $HOME/.zshrc && ln -s $PWD/zsh/.zshrc $HOME/.zshrc
 touch $HOME/.zsh_custom
 
 brew install chruby --HEAD
@@ -23,7 +23,7 @@ brew install ruby-install --HEAD
 ruby-install --latest ruby
 
 brew install tmux
-ln -s tmux/.tmux.conf $HOME/.tmux.conf
+ln -s $PWD/tmux/.tmux.conf $HOME/.tmux.conf
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 nvm install node
