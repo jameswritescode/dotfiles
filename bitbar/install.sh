bb3e661adfe22eeb2200ba3ec9018c67c71202b3
James Newton <hello@jamesnewton.com>
2017-02-13 11:24:29 -0500

    update bitbar install.sh

--- bitbar/install.sh
+++ bitbar/install.sh
@@ -1,2 +1,5 @@
 go build -o enabled/twitch.5m.cgo twitch.5m.go
-ln -s $PWD/semaphore.15m.js $PWD/enabled/semaphore.15m.js
+ln -s $PWD/twitch.5m.go $PWD/enabled/twitch.5m.cgo
+
+go build -o enabled/semaphore.5m.cgo semaphore.5m.go
+ln -s $PWD/semaphore.5m.go $PWD/enabled/semaphore.5m.cgo
