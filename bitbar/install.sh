b0145614f4cd1fe377ea9aab5474beba8173a487
James Newton <hello@jamesnewton.com>
2017-02-13 16:52:45 -0500

    add github bitbar ext

--- bitbar/install.sh
+++ bitbar/install.sh
@@ -1,5 +1,3 @@
 go build -o enabled/twitch.5m.cgo twitch.5m.go
-ln -s $PWD/twitch.5m.go $PWD/enabled/twitch.5m.cgo
-
 go build -o enabled/semaphore.5m.cgo semaphore.5m.go
-ln -s $PWD/semaphore.5m.go $PWD/enabled/semaphore.5m.cgo
+go build -o enabled/github.5m.cgo github.5m.go
