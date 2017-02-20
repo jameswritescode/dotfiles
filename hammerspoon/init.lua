be9c01091d755ba48942d054fa59a185d91716ca
James Newton <hello@jamesnewton.com>
2017-02-20 13:01:52 -0500

    use ctrl+cmd-hjkl rather than ctrl-hjkl - breaks vim/tmux setup

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,14 +1,12 @@
-local function keyCode(key, modifiers)
-  modifiers = modifiers or {}
-
+local function keyCode(key)
   return function()
-    hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+    hs.eventtap.event.newKeyEvent({}, string.lower(key), true):post()
     hs.timer.usleep(1000)
-    hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
+    hs.eventtap.event.newKeyEvent({}, string.lower(key), false):post()
   end
 end
 
-hs.hotkey.bind({'ctrl'}, 'h', keyCode('left'), nil, keyCode('left'))
-hs.hotkey.bind({'ctrl'}, 'j', keyCode('down'), nil, keyCode('down'))
-hs.hotkey.bind({'ctrl'}, 'k', keyCode('up'), nil, keyCode('up'))
-hs.hotkey.bind({'ctrl'}, 'l', keyCode('right'), nil, keyCode('right'))
+hs.hotkey.bind({'ctrl', 'cmd'}, 'h', keyCode('left'), nil, keyCode('left'))
+hs.hotkey.bind({'ctrl', 'cmd'}, 'j', keyCode('down'), nil, keyCode('down'))
+hs.hotkey.bind({'ctrl', 'cmd'}, 'k', keyCode('up'), nil, keyCode('up'))
+hs.hotkey.bind({'ctrl', 'cmd'}, 'l', keyCode('right'), nil, keyCode('right'))
