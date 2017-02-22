0d1144d2caa31df09e479b770807d8de4ce5748a
James Newton <hello@jamesnewton.com>
2017-02-22 16:24:03 -0500

    change hammerspoon keybind

--- hammerspoon/arrows.lua
+++ hammerspoon/arrows.lua
@@ -6,7 +6,7 @@ local function keyCode(key)
   end
 end
 
-hs.hotkey.bind({'ctrl', 'cmd'}, 'h', keyCode('left'), nil, keyCode('left'))
-hs.hotkey.bind({'ctrl', 'cmd'}, 'j', keyCode('down'), nil, keyCode('down'))
-hs.hotkey.bind({'ctrl', 'cmd'}, 'k', keyCode('up'), nil, keyCode('up'))
-hs.hotkey.bind({'ctrl', 'cmd'}, 'l', keyCode('right'), nil, keyCode('right'))
+hs.hotkey.bind(hyper, 'h', keyCode('left'), nil, keyCode('left'))
+hs.hotkey.bind(hyper, 'j', keyCode('down'), nil, keyCode('down'))
+hs.hotkey.bind(hyper, 'k', keyCode('up'), nil, keyCode('up'))
+hs.hotkey.bind(hyper, 'l', keyCode('right'), nil, keyCode('right'))
