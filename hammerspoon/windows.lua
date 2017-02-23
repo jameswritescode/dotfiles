470f38d078105dfe08b5e02cf92320c96ff8eced
James Newton <hello@jamesnewton.com>
2017-02-23 17:24:10 -0500

    no functionality, but some additions to windows.lua

--- hammerspoon/windows.lua
+++ hammerspoon/windows.lua
@@ -1,3 +1,20 @@
+function gethostname()
+  local f = io.popen('/bin/hostname')
+  local hostname = f:read('*a')
+
+  f:close()
+
+  return string.gsub(hostname, '\n$', '')
+end
+
+local screens = {}
+screens['James-Newton-MBP-475.local'] = { 'Color LCD', 'DELL P2715Q' }
+
+local currentScreen = screens[gethostname()]
+
+function minFrame(screen)
+end
+
 local pressed = {
   up = false,
   down = false,
