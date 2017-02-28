cf3eab16badce1451a47539ed39bbe391fd8cb13
James Newton <hello@jamesnewton.com>
2017-02-28 16:13:44 -0500

    add hyper+i to show the grid for a window

--- hammerspoon/windows.lua
+++ hammerspoon/windows.lua
@@ -22,55 +22,39 @@ function setScreen(cb)
 end
 
 hs.hotkey.bind(hyper, 'w', function()
-  pressed.up = true
-
   setScreen(function(cell)
     cell.x = 0
     cell.y = 0
     cell.w = 24
     cell.h = 12
   end)
-end, function()
-  pressed.up = false
 end)
 
 hs.hotkey.bind(hyper, 'a', function()
-  pressed.left = true
-
   setScreen(function(cell)
     cell.x = 0
     cell.y = 0
     cell.w = 12
     cell.h = 24
   end)
-end, function()
-  pressed.left = false
 end)
 
 hs.hotkey.bind(hyper, 's', function()
-  pressed.down = true
-
   setScreen(function(cell)
     cell.x = 0
     cell.y = 12
     cell.w = 24
     cell.h = 12
   end)
-end, function()
-  pressed.down = false
 end)
 
 hs.hotkey.bind(hyper, 'd', function()
-  pressed.right = true
-
   setScreen(function(cell)
     cell.x = 12
     cell.y = 0
     cell.w = 12
     cell.h = 24
   end)
-end, function()
-  pressed.right = false
 end)
 
 hs.hotkey.bind(hyper, 'f', function()
@@ -80,4 +64,12 @@ hs.hotkey.bind(hyper, 'f', function()
     cell.w = 24
     cell.h = 24
   end)
+end)
+
+hs.hotkey.bind(hyper, 'i', function()
+  local win = hs.window.frontmostWindow()
+  local screen = win:screen()
+  local cell = hs.grid.get(win, screen)
+
+  hs.alert.show(cell)
 end)
