dd0b62d0abb369c4f35572c292d956048e9dde82
James Newton <hello@jamesnewton.com>
2017-03-03 14:05:17 -0500

    Add RUST_SRC_PATH assignment to setup

--- workstation.sh
+++ workstation.sh
@@ -1,4 +1,4 @@
-puts "Setting up Workstation"
+puts "Setting up Workstation..."
 
 defaults write com.apple.dock autohide -bool true
 defaults write NSGlobalDomain KeyRepeat -int 1
@@ -8,32 +8,44 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 10
 brew tap homebrew/bundle
 brew bundle
 
-ln -s $PWD/.gitconfig $HOME/.gitconfig
-
-ln -s $PWD/vim/.vimrc $HOME/.vimrc
-ln -s $PWD/vim $HOME/.vim
-git clone http://github.com/VundleVim/Vundle.vim vim/bundle/Vundle.vim
-vim +PluginInstall +qa
-
+# zsh
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 rm $HOME/.zshrc && ln -s $PWD/zsh/.zshrc $HOME/.zshrc
 touch $HOME/.zsh_custom
 
+# git
+ln -s $PWD/.gitconfig $HOME/.gitconfig
+
+# vim
+ln -s $PWD/vim/.vimrc $HOME/.vimrc
+ln -s $PWD/vim $HOME/.vim
+git clone http://github.com/VundleVim/Vundle.vim vim/bundle/Vundle.vim
+vim +PluginInstall +qa
+
+# hammerspoon
 ln -s $PWD/hammerspoon $HOME/.hammerspoon
 
+# ruby
 ruby-install --latest ruby
 chruby ruby
 ruby -v | awk {'print "chruby " $2'} >> $HOME/.zsh_custom
 gem install bundler
 gem install tmuxinator
 
+# tmux
 ln -s $PWD/tmux/.tmux.conf $HOME/.tmux.conf
 
+# node
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 nvm install node
 
+# rust
 curl https://sh.rustup.rs -sSf | sh
 rustup component add rust-src
+rust_toolchain=`rustup toolchain list | awk {'print $1'}`
+echo "export RUST_SRC_PATH=\"\$HOME/.multirust/toolchains/$rust_toolchain/lib/rustlib/src/rust/src\"" >> $HOME/.zsh_custom
 cargo install rustfmt
 cargo install racer
+
+echo "Workstation setup complete."
