b7ef4792781349b75ab863f9d9d12427ec2d0ae5
James Newton <hello@jamesnewton.com>
2018-08-07 21:39:43 -0700

    add gpg-agent config (points to pinentry-mac)

--- workstation.sh
+++ workstation.sh
@@ -7,47 +7,50 @@ sudo defaults write NSGlobalDomain KeyRepeat -int 1
 sudo defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-mkdir -p $HOME/.config
-ln -s $DOTFILES/kitty.conf $HOME/.config/kitty.conf
+mkdir -p "$HOME/.config"
+ln -s "$DOTFILES/kitty.conf" "$HOME/.config/kitty.conf"
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap homebrew/bundle
 brew bundle
 
+# gpg
+cp "$DOTFILES/gpg-agent.conf" "$HOME/.gnupg/gpg-agent.conf"
+
 # git
-ln -s $DOTFILES/git/.gitconfig $HOME/.gitconfig
+ln -s "$DOTFILES/git/.gitconfig" "$HOME/.gitconfig"
 
 # zsh
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-rm $HOME/.zshrc && ln -s $DOTFILES/zsh/.zshrc $HOME/.zshrc
-touch $HOME/.zsh_custom
+rm "$HOME/.zshrc" && ln -s "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
+touch "$HOME/.zsh_custom"
 zplug install
 
 # ctags
-ln -s $DOTFILES/.ctags $HOME/.ctags
+ln -s "$DOTFILES/.ctags" "$HOME/.ctags"
 
 # hammerspoon
-ln -s $DOTFILES/hammerspoon $HOME/.hammerspoon
+ln -s "$DOTFILES/hammerspoon" "$HOME/.hammerspoon"
 
 # ruby
 ruby-install --latest ruby
 chruby ruby
-ruby -v | awk {'print "! [ -f \"$PWD/.ruby-version\" ] && chruby " $2'} >> $HOME/.zsh_custom
+ruby -v | awk {'print "! [ -f \"$PWD/.ruby-version\" ] && chruby " $2'} >> "$HOME/.zsh_custom"
 gem install gem-ctags
 gem ctags
 gem install bundler tmuxinator rubocop reek
 
 if  [[ "$OSTYPE" == "darwin"* ]]; then
-  local BUNDLE_JOBS=`expr $(sysctl -n hw.ncpu) - 1`
+  BUNDLE_JOBS=$(($(sysctl -n hw.ncpu) - 1))
 else
-  local BUNDLE_JOBS=`expr $(nproc) - 1`
+  BUNDLE_JOBS=$(($(nproc) - 1))
 fi
 
 bundle config --global jobs $BUNDLE_JOBS
 
 # tmux
-ln -s $DOTFILES/tmux/.tmux.conf $HOME/.tmux.conf
+ln -s "$DOTFILES/tmux/.tmux.conf" "$HOME/.tmux.conf"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # node
@@ -58,8 +61,8 @@ nvm install node
 curl https://sh.rustup.rs -sSf | sh
 rustup component add rust-src
 rustup component add rust-docs
-rust_toolchain=`rustup toolchain list | awk {'print $1'}`
-echo "export RUST_SRC_PATH=\"\$HOME/.multirust/toolchains/$rust_toolchain/lib/rustlib/src/rust/src\"" >> $HOME/.zsh_custom
+rust_toolchain=$(rustup toolchain list | awk {'print $1'})
+echo "export RUST_SRC_PATH=\"\$HOME/.multirust/toolchains/$rust_toolchain/lib/rustlib/src/rust/src\"" >> "$HOME/.zsh_custom"
 cargo install rustfmt
 cargo install racer
 
@@ -67,9 +70,9 @@ cargo install racer
 go get -u github.com/nsf/gocode
 
 # neovim
-mkdir -p $HOME/dotfiles/vim/bundle/repos/github.com/Shougo/dein.vim
+mkdir -p "$HOME/dotfiles/vim/bundle/repos/github.com/Shougo/dein.vim"
 git clone https://github.com/Shougo/dein.vim vim/bundle/repos/github.com/Shougo/dein.vim
-ln -s $DOTFILES/vim $HOME/.config/nvim
+ln -s "$DOTFILES/vim" "$HOME/.config/nvim"
 ./vim/hosts.sh install
 
 echo "Workstation setup complete."
