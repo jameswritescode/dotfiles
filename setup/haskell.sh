d3bb0d69faae72dc141170c9982099a0b1d16d3e
James Newton <hello@jamesnewton.com>
2018-08-11 17:56:32 -0700

    add haskell setup for tools

---
+++ setup/haskell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+brew install ghc haskell-stack
+ln -s "$DOTFILES/.ghci" "$HOME/.ghci"
+
+clone https://github.com/haskell/haskell-ide-engine --recursive
+cd haskell-ide-engine
+stack install cabal-install
+make build-all
