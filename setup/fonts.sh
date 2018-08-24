af2b2b02434b4a812bb3e63fe5eb213cd39fa389
James Newton <hello@jamesnewton.com>
2018-08-24 02:31:55 -0700

    and devicon support for ranger :>

--- setup/fonts.sh
+++ setup/fonts.sh
@@ -3,13 +3,26 @@
 brew cask install font-fira-code
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  git clone git@github.com:ryanoasis/nerd-fonts.git /tmp/nerd-fonts
+  nf_tmp="/tmp/nerd-fonts"
 
-  pushd /tmp/nerd-fonts || exit
+  git clone git@github.com:ryanoasis/nerd-fonts.git $nf_tmp
+
+  pushd $nf_tmp || exit
     find /Users/james/Library/Fonts -name 'Fira*.ttf' | awk '{print $1}' | xargs -n 1 ./font-patcher
     mv ./*.ttf "$HOME/Desktop"
-    rm -r nerd-fonts
   popd || exit
 
+  rm -r $nf_tmp
+
   open "$HOME"/Desktop/*.ttf
+
+  rd_tmp="/tmp/ranger_devicons"
+
+  git clone https://github.com/alexanderjeurissen/ranger_devicons $rd_tmp
+
+  pushd $rd_tmp || exit
+    make install
+  popd || exit
+
+  rm -r $rd_tmp
 fi
