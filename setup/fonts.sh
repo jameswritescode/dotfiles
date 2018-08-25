a61e56546f2b1642dfd9cb379fe0dfaf4299ab3b
James Newton <hello@jamesnewton.com>
2018-08-25 13:36:04 -0700

    tons more nerd font mappings for ranger

--- setup/fonts.sh
+++ setup/fonts.sh
@@ -5,10 +5,13 @@ brew cask install font-fira-code
 if [[ "$OSTYPE" == "darwin"* ]]; then
   nf_tmp="/tmp/nerd-fonts"
 
-  git clone git@github.com:ryanoasis/nerd-fonts.git $nf_tmp
+  git clone --depth 1 git@github.com:ryanoasis/nerd-fonts.git $nf_tmp
 
   pushd $nf_tmp || exit
-    find /Users/james/Library/Fonts -name 'Fira*.ttf' | awk '{print $1}' | xargs -n 1 ./font-patcher
+    find /Users/james/Library/Fonts -name 'Fira*.ttf' |
+      awk '{print $1}' |
+      xargs -n 1 ./font-patcher -c
+
     mv ./*.ttf "$HOME/Desktop"
   popd || exit
 
