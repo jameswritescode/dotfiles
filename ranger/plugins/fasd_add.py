6e85706bc4f91c5ec57ce4000b5ccde9275964a2
James Newton <hello@jamesnewton.com>
2018-08-25 02:21:11 -0700

    add ranger configs

---
+++ ranger/plugins/fasd_add.py
@@ -0,0 +1,20 @@
+import subprocess
+
+import ranger.api
+from ranger.ext.spawn import check_output
+
+HOOK_INIT_OLD = ranger.api.hook_init
+
+def hook_init(fm):
+    def fasd_add():
+        for fobj in fm.thistab.get_selection():
+            try:
+                check_output(['fasd', '--add', fobj.path])
+            except subprocess.CalledProcessError:
+                pass
+
+    fm.signal_bind('execute.before', fasd_add)
+
+    return HOOK_INIT_OLD(fm)
+
+ranger.api.hook_init = hook_init
