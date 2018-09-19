eb57cc871dc3b91934d5efe4b15f668e5693f943
James Newton <hello@jamesnewton.com>
2018-09-18 19:00:07 -0700

    add python setup, extra Brewfile extraction

--- setup/git.sh
+++ setup/git.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-brew install git hub git-extras
+brew install git hub git-extras diff-so-fancy
 ln -s "$DOTFILES/git/.gitconfig" "$HOME/.gitconfig"
---
+++ setup/python.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+brew install python python3
+
+pip3 install python-language-server
