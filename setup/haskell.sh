6448f3c94f9e4abd46afe731dcbdc195e4ca284f
James Newton <hello@jamesnewton.com>
2018-11-25 23:07:17 -0800

    langserver installation scripting

--- setup/haskell.sh
+++ setup/haskell.sh
@@ -3,8 +3,4 @@
 brew install ghc haskell-stack
 ln -s "$DOTFILES/.ghci" "$HOME/.ghci"
 
-clone https://github.com/haskell/haskell-ide-engine --recursive
-cd haskell-ide-engine
-stack install cabal-install
 stack install hlint
-make build-all
--- setup/neovim.sh
+++ setup/neovim.sh
@@ -1,26 +1,50 @@
 #!/bin/bash
 
+VIMPATH="$DOTFILES/vim"
+
+LANGSERVERPATH="$VIMPATH/langserver"
+HASKELL_SERVER="haskell-ide-engine"
+PYTHON_SERVER="python-language-server[all]"
+
+langserver_haskell_cmds() {
+  pushd "$LANGSERVERPATH/haskell-ide-engine" || exit
+    stack install cabal-install
+    make build-all
+  popd || exit
+}
+
 install() {
-  # neovim
+  # Neovim Setup
   brew tap neovim/neovim
   brew install neovim
-  mkdir -p "$HOME/dotfiles/vim/bundle/repos/github.com/Shougo/dein.vim"
-  git clone https://github.com/Shougo/dein.vim vim/bundle/repos/github.com/Shougo/dein.vim
-  ln -s "$DOTFILES/vim" "$HOME/.config/nvim"
+  curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > /tmp/installer.sh
+  sh /tmp/installer.sh "$VIMPATH/bundle"
 
-  # hosts
-  gem install neovim
-  npm install -g neovim
+  ln -s "$VIMPATH" "$HOME/.config/nvim"
+
+  # Neovim Python Host Setup
   pip3 install virtualenv
-  virtualenv -p python2 "$DOTFILES/vim/virtual/python2"
-  "$DOTFILES"/vim/virtual/python2/bin/pip install neovim
-  virtualenv -p python3 "$DOTFILES/vim/virtual/python3"
-  "$DOTFILES"/vim/virtual/python3/bin/pip install neovim
+
+  virtualenv -p python2 "$VIMPATH/virtual/python2"
+  "$VIMPATH"/virtual/python2/bin/pip install neovim
+
+  virtualenv -p python3 "$VIMPATH/virtual/python3"
+  "$VIMPATH"/virtual/python3/bin/pip install neovim
+
+  # Language Servers
+  mkdir -p "$LANGSERVERPATH"
+
+  pip3 install "$PYTHON_SERVER"
+
+  git clone "https://github.com/haskell/$HASKELL_SERVER" "$LANGSERVERPATH/$HASKELL_SERVER" --recursive
+  langserver_haskell_cmds
 }
 
 update() {
-  for dir in "$DOTFILES"/vim/virtual/python*/; do
+  # Hosts
+  for dir in "$VIMPATH"/virtual/python*/; do
     pushd "$dir" || exit
+      # shellcheck disable=SC1091
       source bin/activate
       pip install --upgrade neovim
       deactivate
@@ -29,6 +53,14 @@ update() {
 
   gem update neovim
   npm install -g neovim
+
+  # Language Servers
+  pip3 install --upgrade "$PYTHON_SERVER"
+
+  pushd "$LANGSERVERPATH/$HASKELL_SERVER" || exit
+    git pull
+    langserver_haskell_cmds
+  popd || exit
 }
 
 case "$1" in
