70e616d77571b7b3d0f4bd0b71c58c0f291fa83e
James Newton <hello@jamesnewton.com>
2019-03-13 14:06:40 -0700

    add haskell-ide-engine setup to setup/haskell

--- setup/haskell.sh
+++ setup/haskell.sh
@@ -4,3 +4,14 @@ brew install ghc haskell-stack
 ln -s "$DOTFILES/.ghci" "$HOME/.ghci"
 
 stack install hlint
+
+HIE_PATH="/tmp/haskell-ide-engine"
+
+git clone git@github.com:haskell/haskell-ide-engine.git $HIE_PATH
+
+pushd $HIE_PATH || exit
+  GHC_VERSION=$(ghc -V | grep -Eo '(?:\d\.?){3}')
+
+  stack ./install.hs "cabal-hie-$GHC_VERSION"
+  stack ./install.hs "cabal-build-doc-$GHC_VERSION"
+popd || exit
