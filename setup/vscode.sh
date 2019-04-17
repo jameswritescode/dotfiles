b0fc495f2a4498f6f187f7c1012a40e1f61b71ab
James Newton <hello@jamesnewton.com>
2019-04-17 14:11:48 -0700

    add code calls to install plugins for vscode

--- setup/vscode.sh
+++ setup/vscode.sh
@@ -5,3 +5,10 @@ brew cask install visual-studio-code
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 
 ln -s "$DOTFILES/vscode.json" "$HOME/Library/Application Support/Code/User/settings.json"
+
+code () {
+  "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" "$@"
+}
+
+code --install-extension castwide.solargraph
+code --install-extension vscodevim.vim
