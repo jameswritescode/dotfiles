b9a5cacdc38c11b6a321c6935f899c7892f5ad20
James Newton <hello@jamesnewton.com>
2019-04-23 10:24:55 -0700

    various cleanup

--- tmux/whatpulse.sh
+++ tmux/whatpulse.sh
@@ -1,1 +1,1 @@
-curl -s localhost:3490/v1/unpulsed | sed 's/[:,]/ /g' | awk {'print $6'} | xargs printf "%'.f"
+curl -s localhost:3490/v1/unpulsed | sed 's/[:,]/ /g' | awk '{print $6}' | xargs printf "%'.f"
