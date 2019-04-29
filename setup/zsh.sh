afeddefc162826f219ea998b5a9d6d288d34ef74
James Newton <hello@jamesnewton.com>
2019-04-28 23:36:51 -0700

    switch to antigen

--- setup/zsh.sh
+++ setup/zsh.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-brew install zsh zplug
+brew install zsh antigen
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-rm "$HOME/.zshrc" && ln -s "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
+rm "$HOME/.zshrc"
+ln -s "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
 touch "$HOME/.zsh_custom"
-zplug install
