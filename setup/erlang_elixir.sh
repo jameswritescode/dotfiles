93402858332db3ddb334105f5ac7651451761900
James Newton <hello@jamesnewton.com>
2019-04-29 00:38:11 -0700

    elixir-ls setup

--- setup/erlang_elixir.sh
+++ setup/erlang_elixir.sh
@@ -11,3 +11,16 @@ install_version() {
 
 install_version erlang "$(latest_version erlang)"
 install_version elixir "$(latest_version elixir)"
+
+mix local.hex --force
+mix local.rebar --force
+
+ELS_PATH="$DOTFILES/vim/langserver/elixir-ls"
+
+git clone git@github.com:JakeBecker/elixir-ls.git "$ELS_PATH"
+
+pushd "$ELS_PATH" || exit
+  mix deps.get
+  mix compile
+  mix elixir_ls.release -o "$ELS_PATH/release"
+popd || exit
