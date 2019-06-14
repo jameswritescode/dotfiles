f918a7caca986928977fcbe2537935eba2e88b3a
James Newton <hello@jamesnewton.com>
2019-06-14 11:13:10 -0700

    extract getLatestReleaseURL to a helper and use that for wtf and go-jira

---
+++ setup/helpers/github.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+getLatestReleaseURL() {
+read -r -d '' GQL <<-GQL
+  query LATEST_RELEASE_QUERY {
+    repository(owner: \"$1\", name: \"$2\") {
+      releases(last:1) {
+        nodes {
+          releaseAssets(first:100) {
+            nodes {
+              downloadUrl
+            }
+          }
+        }
+      }
+    }
+  }
+GQL
+
+read -r -d '' JSON <<-JSON
+  {
+    "query": "$(echo "$GQL" | tr -d '\n')"
+  }
+JSON
+
+  curl \
+    -H "Authorization: Bearer $GITHUB_TOKEN" \
+    -X POST \
+    -d "$JSON" \
+    --silent \
+    https://api.github.com/graphql | \
+  jq \
+    -r \
+    ".data.repository.releases.nodes[0].releaseAssets.nodes[] | select(.downloadUrl | contains(\"$3\")) | .downloadUrl"
+}
