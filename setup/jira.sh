f66d2eb7ad421262c1fa8df7481e54066e127faf
James Newton <hello@jamesnewton.com>
2019-06-14 11:13:10 -0700

    extract getLatestReleaseURL to a helper and use that for wtf and go-jira

--- setup/jira.sh
+++ setup/jira.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-brew install go-jira
+# shellcheck source=/dev/null
+source "${BASH_SOURCE%/*}/helpers/github.sh"
+
+wget "$(getLatestReleaseURL go-jira go-jira darwin)"
+
+INSTALLPATH=/usr/local/bin/jira
+
+mv jira-* "$INSTALLPATH"
+chmod +x "$INSTALLPATH"
 
 ln -s "$DOTFILES/.jira.d" "$HOME"
 
--- setup/wtf.sh
+++ setup/wtf.sh
@@ -1,41 +1,10 @@
 #!/bin/bash
 
-read -r -d '' GQL <<-GQL
-  query LATEST_RELEASE_QUERY {
-    repository(owner: \"wtfutil\", name: \"wtf\") {
-      releases(last:1) {
-        nodes {
-          releaseAssets(first:100) {
-            nodes {
-              downloadUrl
-            }
-          }
-        }
-      }
-    }
-  }
-GQL
-
-read -r -d '' JSON <<-JSON
-  {
-    "query": "$(echo "$GQL" | tr -d '\n')"
-  }
-JSON
-
-getLatestReleaseURL() {
-  curl \
-    -H "Authorization: Bearer $GITHUB_TOKEN" \
-    -X POST \
-    -d "$JSON" \
-    --silent \
-    https://api.github.com/graphql | \
-  jq \
-    -r \
-    '.data.repository.releases.nodes[0].releaseAssets.nodes[] | select(.downloadUrl | contains("darwin_amd64")) | .downloadUrl'
-}
+# shellcheck source=/dev/null
+source "${BASH_SOURCE%/*}/helpers/github.sh"
 
 pushd /tmp || exit
-  wget "$(getLatestReleaseURL)"
+  wget "$(getLatestReleaseURL wtfutil wtf darwin_amd64)"
   tar zxf wtf_*.gz
 
   pushd wtf_*/ || exit
