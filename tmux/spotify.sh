338cc5cc1a90c1661306e65a6345b820459811cb
James Newton <hello@jamesnewton.com>
2019-09-23 15:00:34 -0700

    truncate spotify.sh output if it goes over 30 chars

--- tmux/spotify.sh
+++ tmux/spotify.sh
@@ -1,1 +1,8 @@
-osascript "$DOTFILES/tmux/spotify.applescript" | tr -d '\n'
+MAXLEN=30
+playing=$(osascript "$DOTFILES/tmux/spotify.applescript" | tr -d '\n')
+
+if [ ${#playing} -gt $MAXLEN ]; then
+  echo "$(echo "$playing" | cut -c -$MAXLEN)..."
+else
+  echo "$playing"
+fi
