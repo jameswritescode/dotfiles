3a1082b264e4e7ce05ba47787d9e10fa55aae08c
James Newton <hello@jamesnewton.com>
2020-02-05 12:50:21 -0800

    rust setup updates

--- setup/rust.sh
+++ setup/rust.sh
@@ -3,7 +3,7 @@
 curl https://sh.rustup.rs -sSf | sh
 
 rustup toolchain add nightly
-rustup component add rust-src rls-preview rustfmt-preview clippy-preview
+rustup component add rust-src rls-preview rust-analysis rustfmt-preview clippy-preview
 
 rust_toolchain=$(rustup toolchain list | awk '{print $1}')
 echo "export RUST_SRC_PATH=\"\$HOME/.multirust/toolchains/$rust_toolchain/lib/rustlib/src/rust/src\"" >> "$HOME/.zsh_custom"
