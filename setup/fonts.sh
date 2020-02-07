11d69871ba2b88f343d775b941dd91ab6c5f56b0
James Newton <hello@jamesnewton.com>
2020-02-07 13:33:51 -0800

    update setup/fonts

--- setup/fonts.sh
+++ setup/fonts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-brew cask install font-fira-code font-jetbrains-mono
+brew cask reinstall font-fira-code font-jetbrains-mono
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   nf_tmp="/tmp/nerd-fonts"
@@ -8,14 +8,13 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   git clone --depth 1 git@github.com:ryanoasis/nerd-fonts.git $nf_tmp
 
   pushd $nf_tmp || exit
-    find /Users/james/Library/Fonts -name 'Fira*.ttf' |
-      awk '{print $1}' |
+    find -E /Users/james/Library/Fonts -regex '.*(Jet|Fira).*\.ttf' |
       xargs -n 1 ./font-patcher -c
 
     mv ./*.ttf "$HOME/Desktop"
   popd || exit
 
-  rm -r $nf_tmp
+  rm -rf $nf_tmp
 
   open "$HOME"/Desktop/*.ttf
 fi
