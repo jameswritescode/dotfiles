ecc31040ccee3b4f5f42b5cee6b7856231143904
James Newton <hello@jamesnewton.com>
2020-03-24 09:43:02 -0700

    add iterm shell integration snippet

--- zsh/partials/tools.zsh
+++ zsh/partials/tools.zsh
@@ -1,13 +1,19 @@
+# asdf
 export NODEJS_CHECK_SIGNATURES="no"
 ASDF_BREW_PATH=$(brew --prefix asdf)
 source $ASDF_BREW_PATH/asdf.sh
 source $ASDF_BREW_PATH/etc/bash_completion.d/asdf.bash
 
+# fasd
 eval "$(fasd --init zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install)"
 
+# fzf
 export FZF_DEFAULT_COMMAND='fd -t f -H -E .git'
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 # antigen
 source /usr/local/share/antigen/antigen.zsh
 antigen apply
+
+# iterm2
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
