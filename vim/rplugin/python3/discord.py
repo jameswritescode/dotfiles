be8a45339beccf37cef8ed4dbba983d5b0f7b246
James Newton <hello@jamesnewton.com>
2020-04-06 00:12:21 -0700

    add erb to discord.py

--- vim/rplugin/python3/discord.py
+++ vim/rplugin/python3/discord.py
@@ -111,6 +111,7 @@ class RPCClient:
 class DiscordPlugin:
     FT_OVERRIDES = {
         'dockerfile': 'docker',
+        'eruby': 'ruby',
         'eruby.yaml': 'yaml',
         'gitcommit': 'git',
         'javascriptreact': 'react',
