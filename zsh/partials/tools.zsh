fbaf35757daf9d23cfaf7d5a3f3ffdb625b97d2b
James Newton <hello@jamesnewton.com>
2021-05-20 14:44:35 -0700

    don't source asdf if it doesn't exist

--- zsh/partials/tools.zsh
+++ zsh/partials/tools.zsh
@@ -1,8 +1,11 @@
 # asdf
 export NODEJS_CHECK_SIGNATURES="no"
 ASDF_BREW_PATH=$(brew --prefix asdf)
-source $ASDF_BREW_PATH/asdf.sh
-source $ASDF_BREW_PATH/etc/bash_completion.d/asdf.bash
+
+if [ -f $ASDF_BREW_PATH/asdf.sh ]; then
+  source $ASDF_BREW_PATH/asdf.sh
+  source $ASDF_BREW_PATH/etc/bash_completion.d/asdf.bash
+fi
 
 # fasd
 eval "$(fasd --init zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install)"
