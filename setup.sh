4c8aa57be2edee6f67fbd2b5ccc26ce147b50c84
James Newton <hello@jamesnewton.com>
2021-09-14 17:29:12 -0700

    add zsh to setup

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ elif [[ $(awk -F= '/^NAME/{print $2}' /etc/os-release 2>/dev/null) == '"Ubuntu"'
         sudo apt-get install -y software-properties-common build-essential
 
         ./setup/git.sh install_ubuntu
+        ./setup/zsh.sh install_ubuntu
         ./setup/python.sh install_ubuntu
         ./setup/node.sh install_ubuntu
         ./setup/fzf.sh install_ubuntu
