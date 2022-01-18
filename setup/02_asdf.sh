0397e3d3d5f4bfd467573fb69c6dbe8ba22fda9f
James Newton <hello@jamesnewton.com>
2022-01-18 01:22:26 -0800

    better asdf setup script

--- setup/02_asdf.sh
+++ setup/02_asdf.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-brew install asdf
+case "$1" in
+  install_macos)
+    brew install asdf
 
-asdf plugin-add elixir
-asdf plugin-add erlang
-asdf plugin-add nodejs
-asdf plugin-add ruby
+    ASDF_BREW_PATH=$(brew --prefix asdf 2>/dev/null)
 
-ln -s "$DOTFILES/asdf/.default-gems" "$HOME/.default-gems"
-ln -s "$DOTFILES/asdf/.default-npm-packages" "$HOME/.default-npm-packages"
-ln -s "$DOTFILES/asdf/.asdfrc" "$HOME/.asdfrc"
+    if [ -f $ASDF_BREW_PATH/asdf.sh ]; then
+      echo "source $ASDF_BREW_PATH/asdf.sh" >> "$HOME/.zsh_custom"
+    fi
+  ;;
+
+  install_ubuntu)
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0
+
+    echo "source $HOME/.asdf/asdf.sh" >> "$HOME/.zsh_custom"
+  ;;
+esac
+
+setup() {
+  asdf plugin-add elixir
+  asdf plugin-add erlang
+  asdf plugin-add nodejs
+  asdf plugin-add ruby
+
+  ln -s "$DOTFILES/asdf/.default-gems" "$HOME/.default-gems"
+  ln -s "$DOTFILES/asdf/.default-npm-packages" "$HOME/.default-npm-packages"
+  ln -s "$DOTFILES/asdf/.asdfrc" "$HOME/.asdfrc"
+}
--- zsh/partials/tools.zsh
+++ zsh/partials/tools.zsh
@@ -1,11 +1,5 @@
 # asdf
 export NODEJS_CHECK_SIGNATURES="no"
-ASDF_BREW_PATH=$(brew --prefix asdf 2>/dev/null)
-
-if [ -f $ASDF_BREW_PATH/asdf.sh ]; then
-  source $ASDF_BREW_PATH/asdf.sh
-  source $ASDF_BREW_PATH/etc/bash_completion.d/asdf.bash
-fi
 
 # fasd
 eval "$(fasd --init zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install)"
