23673a90a0ca2f1e0f06844437792eb9d9446d94
James Newton <hello@jamesnewton.com>
2022-05-12 13:31:29 -0700

    unset pager in spin

--- setup/spin.sh
+++ setup/spin.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-curl -s https://api.github.com/repos/dandavison/delta/releases/latest \
-  | grep "browser_download_url.*amd64.deb" \
-  | cut -d : -f2,3 \
-  | head -n1 \
-  | tr -d '"' \
-  | wget -O git-delta.deb -i -
+# curl -s https://api.github.com/repos/dandavison/delta/releases/latest \
+#   | grep "browser_download_url.*amd64.deb" \
+#   | cut -d : -f2,3 \
+#   | head -n1 \
+#   | tr -d '"' \
+#   | wget -O git-delta.deb -i -
 
-sudo dpkg -i git-delta.deb
-rm git-delta.deb
+# sudo dpkg -i git-delta.deb
+# rm git-delta.deb
 
 ./setup/git.sh install_ubuntu
+
+git config --global --unset core.pager
+
 ./setup/zsh.sh install_ubuntu
 
 sudo gem install neovim --no-doc
