854db86a32cf6b31f11900aade1ed1abfa0e42c9
James Newton <hello@jamesnewton.com>
2022-09-28 00:38:27 -0700

    switch to packer

--- vim/init.lua
+++ vim/init.lua
@@ -1,7 +1,5 @@
 vim.cmd('source ~/dotfiles/vim/init-old.vim')
 
-require 'user.cmp'
-require 'user.gitsigns'
-require 'user.lsp'
-require 'user.telescope'
-require 'user.treesitter'
+pcall(vim.cmd, 'colorscheme onedark')
+
+require 'user.packer'
---
+++ vim/lua/user/which-key.lua
@@ -0,0 +1,33 @@
+local which_key = require('which-key')
+
+which_key.setup({
+  disable = {
+    filetypes = { 'TelescopePrompt' },
+  },
+
+  plugins = {
+    marks = false,
+    registers = false,
+
+    presets = {
+      g = false,
+      motions = false,
+      nav = false,
+      operators = false,
+      windows = false,
+      z = false,
+    },
+  },
+
+  ignore_missing = true,
+})
+
+which_key.register(
+  vim.g.which_key_map,
+  { prefix = '<leader>' }
+)
+
+which_key.register(
+  vim.g.which_key_visual_map,
+  { mode = 'v', prefix = '<leader>' }
+)
