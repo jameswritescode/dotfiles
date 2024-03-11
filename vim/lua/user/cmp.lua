b69c7c44bc3c45cd2bbcc3bb172bb7c9c80ab997
James Newton <hello@jamesnewton.com>
2024-03-11 00:59:57 -0700

    update lua_ls config, remove has_words_before from cmp

--- vim/lua/user/cmp.lua
+++ vim/lua/user/cmp.lua
@@ -3,15 +3,9 @@ local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 -- local copilot_cmp = require('copilot_cmp.comparators')
 local luasnip = require('luasnip')
 
-local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
-end
-
 cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
 
-cmp.setup{
+cmp.setup {
   formatting = {
     format = require('lspkind').cmp_format({
       mode = 'symbol',
@@ -44,7 +38,7 @@ cmp.setup{
     end, { 'i', 's' }),
 
     ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() and has_words_before() then
+      if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
