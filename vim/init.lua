1fa086147e8891b9a863a9f78491796f0bfbda85
James Newton <hello@jamesnewton.com>
2024-03-10 23:20:12 -0700

    various lsp/config updates

--- vim/init.lua
+++ vim/init.lua
@@ -1,5 +1,6 @@
 vim.cmd('source ~/dotfiles/vim/init-old.vim')
 
 require 'user.lazy'
+require 'user.lsp'
 
 vim.cmd.colorscheme 'catppuccin'
--- vim/lua/user/lsp/common.lua
+++ vim/lua/user/lsp/common.lua
@@ -2,74 +2,12 @@ local cmp = require 'cmp'
 
 local common = {}
 
--- TODO: Is there a better way to do this now?
-function lsp_diagnostic_hover_hack()
-  vim.o.eventignore = 'CursorHold'
-  vim.lsp.buf.hover()
-  vim.cmd([[autocmd CursorMoved <buffer> ++once set eventignore=""]])
-end
-
 common.window_opts = cmp.config.window.bordered()
 
 function common.set_winhighlight(winnr)
   if not winnr then return end
 
   vim.wo[winnr].winhl = common.window_opts.winhighlight
-end
-
-local formatting_augroup = vim.api.nvim_create_augroup('LspFormatting', {})
-
-function common.on_attach_formatting(client, bufnr)
-  if client.supports_method('textDocument/formatting') then
-    vim.api.nvim_clear_autocmds({ group = formatting_augroup, buffer = bufnr })
-
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      group = formatting_augroup,
-      buffer = bufnr,
-      callback = function()
-        vim.lsp.buf.format({ bufnr = bufnr })
-      end,
-    })
-  end
-end
-
-function common.on_attach(client, bufnr)
-  local map_opts = { noremap = true, silent = true, buffer = bufnr }
-
-  vim.keymap.set('n', 'K', '<cmd>lua lsp_diagnostic_hover_hack()<CR>', map_opts)
-
-  vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, map_opts)
-  vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, map_opts)
-  vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, map_opts)
-
-  vim.keymap.set('n', '<leader>lgd', '<cmd>Telescope lsp_definitions<CR>', map_opts)
-  vim.keymap.set('n', '<leader>lgi', '<cmd>Telescope lsp_implementations<CR>', map_opts)
-  vim.keymap.set('n', '<leader>lgr', '<cmd>Telescope lsp_references<CR>', map_opts)
-  vim.keymap.set('n', '<leader>lgy', '<cmd>Telescope lsp_type_definitions<CR>', map_opts)
-
-  common.on_attach_formatting(client, bufnr)
-
-  vim.api.nvim_create_autocmd('CursorHold', {
-    buffer = bufnr,
-
-    callback = function()
-      local opts = {
-        border = 'rounded',
-        close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter', 'FocusLost' },
-        focusable = false,
-        scope = 'cursor',
-        source = 'always',
-
-        format = function(diagnostic)
-          return string.format("[%s] %s", diagnostic.code, diagnostic.message)
-        end
-      }
-
-      local _, winnr = vim.diagnostic.open_float(opts)
-
-      common.set_winhighlight(winnr)
-    end,
-  })
 end
 
 return common
