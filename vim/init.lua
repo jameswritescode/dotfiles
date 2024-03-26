c77d49af5409375f9021545aa1fd9ef42167369c
James Newton <hello@jamesnewton.com>
2024-03-25 21:41:40 -0500

    statusline updates

--- vim/init.lua
+++ vim/init.lua
@@ -1,5 +1,6 @@
 vim.cmd('source ~/dotfiles/vim/init-old.vim')
 
+require 'user.options'
 require 'user.lazy'
 require 'user.keymaps'
 require 'user.lsp'
@@ -9,5 +10,3 @@ vim.cmd.colorscheme 'catppuccin'
 require 'user.statusline'
 require 'user.tabline'
 require 'user.winbar'
-
-vim.o.cmdheight = 0
