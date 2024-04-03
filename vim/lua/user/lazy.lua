fe6de4310f8be5f2e1307d29316f75655d406759
James Newton <hello@jamesnewton.com>
2024-04-03 16:49:45 -0700

    change devicons ruby color to make it more readable

--- vim/lua/user/lazy.lua
+++ vim/lua/user/lazy.lua
@@ -269,8 +269,28 @@ require('lazy').setup({
 
   {
     'nvim-tree/nvim-web-devicons',
-    config = true,
     lazy = false,
+    config = function()
+      local devicons = require('nvim-web-devicons')
+      local icons = devicons.get_icons()
+
+      local ruby_types = {
+        'brewfile',
+        'config.ru',
+        'erb',
+        'gemfile$',
+        'gemspec',
+        'rake',
+        'rakefile',
+        'rb',
+      }
+
+      for _, n in pairs(ruby_types) do
+        icons[n].color = '#cc342d'
+      end
+
+      devicons.setup()
+    end,
   },
 
   { 'AndrewRadev/switch.vim',         event = 'VeryLazy' },
