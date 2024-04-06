13680d4fd63ffc95a24c848af902e5f66ca2fc3c
James Newton <hello@jamesnewton.com>
2024-04-05 18:11:06 -0700

    clean up unused dirs in vim/

--- vim/template/app/jobs/template_job.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class <+CURSOR+>Job
-  include Sidekiq::Worker
-
-  def perform
-
-  end
-end
--- vim/template/template.jsx
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import React, { type Node } from 'react';
-
-export default function <+CURSOR+>(): Node {
-  return (
-    <h1>hello world</h1>
-  );
-}
