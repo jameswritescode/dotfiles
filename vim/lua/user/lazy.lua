8ff69cd6d4859e67dab5adf7cfd5bb3e43c173cd
James Newton <hello@jamesnewton.com>
2024-04-18 16:11:42 -0700

    giving nvim-java a go

--- vim/lua/user/lazy.lua
+++ vim/lua/user/lazy.lua
@@ -90,10 +90,26 @@ require('lazy').setup({
   },
 
   {
-    'mfussenegger/nvim-jdtls',
-    config = function() require('user.lsp.jdtls') end,
-    enabled = false,
-    ft = 'java',
+    'nvim-java/nvim-java',
+    event = 'VeryLazy',
+    dependencies = {
+      'nvim-java/lua-async-await',
+      'nvim-java/nvim-java-core',
+      'nvim-java/nvim-java-test',
+      'nvim-java/nvim-java-dap',
+      'MunifTanjim/nui.nvim',
+      'neovim/nvim-lspconfig',
+      'mfussenegger/nvim-dap',
+      {
+        'williamboman/mason.nvim',
+        opts = {
+          registries = {
+            'github:nvim-java/mason-registry',
+            'github:mason-org/mason-registry',
+          },
+        },
+      },
+    },
   },
 
   -------------
