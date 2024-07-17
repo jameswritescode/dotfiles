dcbf692dd54fca0c63062fd2529ab526c773fc1e
James Newton <hello@jamesnewton.com>
2024-07-17 14:27:20 -0700

    update which-key config

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,10 +1,8 @@
+--# selene: allow(mixed_table)
+
 local which_key = require('which-key')
 
 which_key.setup({
-  disable = {
-    filetypes = { 'TelescopePrompt' },
-  },
-
   plugins = {
     marks = false,
     registers = false,
@@ -14,15 +12,34 @@ which_key.setup({
       motions = false,
       nav = false,
       operators = false,
+      text_objects = true,
       windows = false,
       z = false,
     },
   },
 })
 
-which_key.register(vim.g.which_key_map, { prefix = '<leader>' })
+which_key.add({
+  { '<leader>a', group = 'ai' },
+  { '<leader>b', group = 'buffer' },
+  { '<leader>d', group = 'dap' },
+  { '<leader>f', group = 'fzf' },
+  { '<leader>g', group = 'git' },
+  { '<leader>l', group = 'lsp' },
+  { '<leader>la', desc = 'code-action' },
+  { '<leader>ld', group = 'diagnostic' },
+  { '<leader>lf', desc = 'format' },
+  { '<leader>lg', group = 'goto' },
+  { '<leader>lgd', desc = 'definition' },
+  { '<leader>lgi', desc = 'implementation' },
+  { '<leader>lgr', desc = 'references' },
+  { '<leader>lgy', desc = 'type-definition' },
+  { '<leader>lr', desc = 'rename' },
+  { '<leader>t', group = 'terminal/toggle' },
+})
 
-which_key.register(
-  vim.g.which_key_visual_map,
-  { mode = 'v', prefix = '<leader>' }
-)
+which_key.add({
+  mode = { 'v' },
+  { '<leader>a', group = 'ai' },
+  { '<leader>g', group = 'git' },
+})
--- .config/nvim/partials/mappings.vim
+++ .config/nvim/partials/mappings.vim
@@ -2,37 +2,6 @@ let g:yank_copy_command = trim(system('uname -a')) =~# 'microsoft' ? 'clip.exe' 
 
 vnoremap <expr> <c-c> 'y:call system("' . g:yank_copy_command . '", getreg("\""))<cr>'
 
-""""""""""""""""""""""""""""""
-" which-key related mappings "
-""""""""""""""""""""""""""""""
-
-let g:which_key_map = {}
-let g:which_key_visual_map = {}
-
-let g:which_key_map.a = { 'name': '+ai' }
-let g:which_key_map.b = { 'name': '+buffer' }
-let g:which_key_map.d = { 'name': '+dap' }
-let g:which_key_map.f = { 'name': '+fzf' }
-let g:which_key_map.t = { 'name': '+terminal/toggle' }
-
-" +git
-let g:which_key_map.g = { 'name': '+git' }
-let g:which_key_visual_map.g = { 'name': g:which_key_map.g.name }
-
-" +lsp
-let g:which_key_map.l = { 'name': '+lsp' }
-let g:which_key_map.l.a = 'code-action'
-let g:which_key_map.l.f = 'format'
-let g:which_key_map.l.r = 'rename'
-
-" +diagnostic
-let g:which_key_map.l.d = { 'name': '+diagnostic' }
-let g:which_key_map.l.g = { 'name': '+goto' }
-let g:which_key_map.l.g.d = 'definition'
-let g:which_key_map.l.g.i = 'implementation'
-let g:which_key_map.l.g.r = 'references'
-let g:which_key_map.l.g.y = 'type-definition'
-
 " let g:which_key_map.r = { 'name': 'rest' }
 " let g:which_key_map.r.r = 'run'
 " nnoremap <silent><leader>rr <Plug>RestNvim<cr>
