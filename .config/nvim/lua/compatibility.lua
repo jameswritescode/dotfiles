39952d1fc349e1c46fbd1126add6c9acf2a0836f
James Newton <hello@jamesnewton.com>
2024-08-07 18:49:40 -0700

    compatibility notifications

--- .config/nvim/lua/compatibility.lua
+++ .config/nvim/lua/compatibility.lua
@@ -1,10 +1,19 @@
--- TODO: Remove this after 0.12
-if vim.lsp.get_clients then
-  vim.lsp.get_active_clients = vim.lsp.get_clients
+local function compatibility_override(version, name, fn)
+  if vim.version.gt(vim.version(), version) then
+    vim.notify(
+      'Compatibility removal available for ' .. name,
+      vim.log.levels.WARN
+    )
+  else
+    fn()
+  end
 end
 
--- TODO: Remove after 0.13
-if vim.diagnostic.jump then
+compatibility_override('0.12', 'vim.lsp.get_active_clients', function()
+  vim.lsp.get_active_clients = vim.lsp.get_clients
+end)
+
+compatibility_override('0.13', 'vim.diagnostic.goto_* overrides', function()
   ---@diagnostic disable-next-line: duplicate-set-field
   vim.diagnostic.goto_next = function()
     vim.diagnostic.jump({ count = 1, float = true })
@@ -14,4 +23,4 @@ if vim.diagnostic.jump then
   vim.diagnostic.goto_prev = function()
     vim.diagnostic.jump({ count = -1, float = true })
   end
-end
+end)
