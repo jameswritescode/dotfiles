485f62b9d07ecb67af67ad1dea74c106f3fe76d4
James Newton <hello@jamesnewton.com>
2024-09-27 16:04:09 -0500

    stop setting NVIM_LISTEN_ADDRESS

--- zsh/partials/exports.zsh
+++ zsh/partials/exports.zsh
@@ -7,7 +7,5 @@ export HISTCONTROL="ignoreboth:erasedups"
 export HISTSIZE=500000
 export SAVEHIST=500000
 
-export NVIM_LISTEN_ADDRESS="/tmp/nvimsocket"
-
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
