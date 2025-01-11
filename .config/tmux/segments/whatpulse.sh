973c4c5c277d14377bbcbbde04afd393a80b3293
James Newton <hello@jamesnewton.com>
2025-01-11 01:35:33 -0800

    tmux: adjust status-left

--- .config/tmux/segments/whatpulse.sh
+++ .config/tmux/segments/whatpulse.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 curl -s localhost:3490/v1/unpulsed | jq -r .keys_formatted
--- .config/tmux/status-right.sh
+++ .config/tmux/status-right.sh
@@ -2,15 +2,17 @@
 
 # export TMUX_THEME_SEGMENTS="spotify whatpulse"
 
+SEGMENT_BASE_PATH="$XDG_CONFIG_HOME/tmux/segments"
+
 # TODO can we make the clock a segment, but use tmux's formatting?
 find_segment() {
   case "$1" in
     spotify)
-      echo "#[fg=#a6e3a1] $("$DOTFILES"/.config/tmux/spotify.sh)"
+      echo "#[fg=#a6e3a1] $("$SEGMENT_BASE_PATH/spotify.sh")"
     ;;
 
     whatpulse)
-      echo "#[fg=#89b4fa] $("$DOTFILES"/.config/tmux/whatpulse.sh)"
+      echo "#[fg=#89b4fa] $("$SEGMENT_BASE_PATH/whatpulse.sh")"
     ;;
 
     *)
