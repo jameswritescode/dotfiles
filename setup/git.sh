ecb9aa7790205b045b726f1bf7f100c81e41a6c8
James Newton <hello@jamesnewton.com>
2025-01-24 19:46:22 -0800

    delete gpg setup files

--- setup/git.sh
+++ setup/git.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-ln -sf "$HOME/dotfiles/.config/git" "$HOME/.config/git"
+setup/dotconfig.sh git
 
 CUSTOMFILE="$HOME/.gitconfig_custom"
 
@@ -10,17 +10,16 @@ touch "$CUSTOMFILE"
 touch "$HOME/.gitignore_global"
 
 case "$1" in
-  install_macos)
-    brew install git git-extras git-delta
-  ;;
+    install_macos)
+        brew install git git-extras delta
+        ;;
 
-  install_ubuntu)
-    if [[ -n $SPIN ]]; then
-      git config --file "$CUSTOMFILE" commit.gpgsign false
-      git config --file "$CUSTOMFILE" user.email "james.newton@shopify.com"
-    else
-      sudo add-apt-repository -y ppa:git-core/ppa
-      sudo apt-get install -y git
-    fi
-  ;;
+    install_ubuntu)
+        if [[ -n $SPIN ]]; then
+            git config --file "$CUSTOMFILE" user.email "james.newton@shopify.com"
+        else
+            sudo add-apt-repository -y ppa:git-core/ppa
+            sudo apt-get install -y git
+        fi
+        ;;
 esac
--- setup/gpg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-brew install gpg pinentry-mac
-ln -s "$DOTFILES/gpg-agent.conf" "$HOME/.gnupg/gpg-agent.conf"
-killall gpg-agent
