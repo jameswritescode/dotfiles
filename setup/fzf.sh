15c468b40e415649a5b0fc48ea4ad75f9b2cef3c
James Newton <hello@jamesnewton.com>
2025-01-24 23:07:19 -0800

    update setup/fzf.sh

--- setup/fzf.sh
+++ setup/fzf.sh
@@ -2,18 +2,24 @@
 
 set -x
 
+install_shell_integration() {
+    echo "source <(fzf --zsh)" >> "$HOME/.zsh_custom"
+}
+
 case "$1" in
-  install_macos)
-    brew install fzf --HEAD
+    install_macos)
+        brew install fzf --HEAD
+        install_shell_integration
+        ;;
 
-    "$(brew --prefix)/opt/fzf/install"
-  ;;
+    install_ubuntu)
+        git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
 
-  install_ubuntu)
-    git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
+        "$HOME/.fzf/install" --key-bindings --completion --no-update-rc
 
-    "$HOME/.fzf/install" --key-bindings --completion --no-update-rc
+        install_shell_integration
+        ;;
 
-    echo "source $HOME/.fzf.zsh" >> "$HOME/.zsh_custom"
-  ;;
+    *)
+        echo "install_macos | install_ubuntu"
 esac
