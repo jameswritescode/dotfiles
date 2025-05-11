665243c94044f9375a34d899709637eba564b394
James Newton <hello@jamesnewton.com>
2025-05-11 16:44:54 -0700

    remove on_new_config from yamlls config

--- .config/nvim/after/lsp/yamlls.lua
+++ .config/nvim/after/lsp/yamlls.lua
@@ -1,14 +1,5 @@
 --- @type vim.lsp.Config
 return {
-  -- Does this work anymore?
-  on_new_config = function(new_config)
-    new_config.settings.yaml.schemas = vim.tbl_deep_extend(
-      'force',
-      new_config.settings.yaml.schemas or {},
-      require('schemastore').yaml.schemas()
-    )
-  end,
-
   settings = {
     redhat = {
       telemetry = {
@@ -27,6 +18,8 @@ return {
         enable = false,
         url = '',
       },
+
+      schemas = require('schemastore').yaml.schemas(),
     },
   },
 }
