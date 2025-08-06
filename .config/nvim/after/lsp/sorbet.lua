f4149591d7ae5c81d93ed24cbd4458291a6566b9
James Newton <hello@jamesnewton.com>
2025-08-05 22:25:19 -0700

    Only enable sorbet if there is a match on the root pattern

--- .config/nvim/after/lsp/sorbet.lua
+++ .config/nvim/after/lsp/sorbet.lua
@@ -1,4 +1,13 @@
+local util = require('lspconfig.util')
+
 --- @type vim.lsp.Config
 return {
-  root_markers = { 'sorbet' },
+  root_dir = function(bufnr, on_dir)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    local root_pattern = util.root_pattern('sorbet')(fname)
+
+    if root_pattern then
+      on_dir(root_pattern)
+    end
+  end,
 }
