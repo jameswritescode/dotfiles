9fa77699dd20143d5ad4792cbe03fe31f63b5fac
James Newton <hello@jamesnewton.com>
2025-08-31 14:56:31 -0700

    Define XDG_STATE_HOME if it isn't present

--- zsh/partials/compatibility.zsh
+++ zsh/partials/compatibility.zsh
@@ -1,7 +1,11 @@
+if [[ -z $XDG_CACHE_HOME ]]; then
+    export XDG_CACHE_HOME="$HOME/.cache"
+fi
+
 if [[ -z $XDG_CONFIG_HOME ]]; then
     export XDG_CONFIG_HOME="$HOME/.config"
 fi
 
-if [[ -z $XDG_CACHE_HOME ]]; then
-    export XDG_CACHE_HOME="$HOME/.cache"
+if [[ -z $XDG_STATE_HOME ]]; then
+    export XDG_STATE_HOME="$HOME/.local/state"
 fi
