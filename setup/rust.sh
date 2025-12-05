511a259bc8b25326463419d107c16787ada5b698
James Newton <hello@jamesnewton.com>
2025-12-04 20:19:20 -0600

    fix echo statement

--- setup/rust.sh
+++ setup/rust.sh
@@ -6,4 +6,4 @@ rustup toolchain add stable
 rustup toolchain add nightly
 
 # shellcheck disable=SC2016
-echo 'export PATH="$HOME/.cargo/bin:$PATH' >> "$HOME/.zsh_custom"
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> "$HOME/.zsh_custom"
