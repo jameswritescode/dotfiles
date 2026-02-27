8d88eb9f98e1edf29a5a1742969c484ca0b6207e
James Newton <hello@jamesnewton.com>
2026-02-26 18:38:02 -0800

    pi setup

---
+++ setup/pi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+install_standard() {
+    setup/dotconfig.sh link pi
+
+    mkdir -p "$HOME/.pi"
+    ln -sfn "$HOME/.config/pi" "$HOME/.pi/agent"
+
+    bunx @every-env/compound-plugin install compound-engineering --to pi
+
+    # shellcheck disable=SC2016
+    echo 'export PI_CODING_AGENT_DIR="$HOME/.config/pi"' >> "$HOME/.zsh_custom"
+}
+
+install_standard
