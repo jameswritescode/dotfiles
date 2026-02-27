8ac47f06d8e5dea6138693248e5b54ec70ac04d5
James Newton <hello@jamesnewton.com>
2026-02-27 13:28:13 -0800

    pi setup updates

--- setup/pi.sh
+++ setup/pi.sh
@@ -10,6 +10,8 @@ install_standard() {
 
     bunx @every-env/compound-plugin install compound-engineering --to pi
 
+    pnpm install -g @mariozechner/pi-coding-agent
+
     # shellcheck disable=SC2016
     echo 'export PI_CODING_AGENT_DIR="$HOME/.config/pi"' >> "$HOME/.zsh_custom"
 }
